6782dfd10205d34c5238036ad32bfefa4d9b2991
JU4NSCV <jscueva5@utpl.edu.ec>
2026-04-21 12:11:46 -0500

    version funcional local

---
+++ api/main.py
@@ -0,0 +1,15 @@
+id="m1imp"
+from fastapi import FastAPI
+from base import conectar
+
+app = FastAPI()
+
+@app.get("/empleados")
+def empleados():
+    conn = conectar()
+    cur = conn.cursor()
+    cur.execute("SELECT nombre, departamento, salario FROM empleados")
+    datos = cur.fetchall()
+    conn.close()
+
+    return [{"nombre":d[0],"departamento":d[1],"salario":d[2]} for d in datos]
---
+++ visualizacion/app.py
@@ -0,0 +1,13 @@
+id="a1cfg"
+import streamlit as st
+import requests
+import pandas as pd
+from config import API_URL
+
+st.title("Dashboard")
+
+data = requests.get(f"{API_URL}/empleados").json()
+df = pd.DataFrame(data)
+
+st.dataframe(df)
+st.write(df["salario"].mean())
